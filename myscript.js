58b9e33e06da2f246ef5692f1ff672e0868ed7d4
Leston <129874349+lestonpoh@users.noreply.github.com>
2023-08-14 15:00:26 +0800

    Add code for plus minus multiply divide

--- myscript.js
+++ myscript.js
@@ -2,7 +2,7 @@ function add(num1,num2){
     return num1 + num2
 }
 
-function subtract(num1,num2){
+function minus(num1,num2){
     return num1 - num2
 }
 
@@ -15,11 +15,11 @@ function divide(num1,num2){
 }
 
 function operate(num1,num2,operator){
-    if (operator === add){
+    if (operator === "add"){
         return add(num1,num2)
-    }else if (operator === subtract){
-        return subtract(num1,num2)
-    }else if (operator === multiply){
+    }else if (operator === "minus"){
+        return minus(num1,num2)
+    }else if (operator === "multiply"){
         return multiply(num1,num2)
     }else{
         return divide(num1,num2)
@@ -28,9 +28,14 @@ function operate(num1,num2,operator){
 
 const NUMBERS = {"one":1,"two":2,"three":3,"four":4,"five":5,
                 "six":6,"seven":7,"eight":8,"nine":9,"zero":0};
+const OPERATORS = {"add":"+","minus":"-","divide":"÷","multiply":"×"}
 let displayValue = "";
+let input;
+let currentValue = "";
+let currentOperator;
 
 const displayText = document.querySelector("#displayText")
+const displayTextTop = document.querySelector("#displayTextTop")
 const buttons = document.querySelectorAll("button")
 buttons.forEach((button)=> button.addEventListener("click",()=>{
     if (Object.keys(NUMBERS).includes(button.id)){
@@ -38,11 +43,36 @@ buttons.forEach((button)=> button.addEventListener("click",()=>{
             displayValue+=NUMBERS[button.id]
             displayText.textContent = displayValue
         }
-    }else if (button.id === "clear"){
-        displayValue = ""
-        displayText.textContent = displayValue
+    }else if (button.id === "decimal"){
+        if (displayValue.length <14 && !displayValue.includes(".")){
+            displayValue+="."
+            displayText.textContent = displayValue
+        }
+    }
+    else if (button.id === "clear"){
+        currentValue = "";
+        displayValue = "";
+        displayText.textContent = displayValue;
+        displayTextTop.textContent = currentValue;
     }else if (button.id === "delete"){
         displayValue = displayValue.slice(0,-1);
-        displayText.textContent = displayValue
+        displayText.textContent = displayValue;
+    }else if (Object.keys(OPERATORS).includes(button.id)){
+        input = displayValue;
+        if (input === ""){
+            displayTextTop.textContent = currentValue + " " + OPERATORS[button.id];
+        }else if (currentValue === ""){
+            currentValue = input;
+            displayTextTop.textContent = currentValue + " " + OPERATORS[button.id];
+            displayValue = "";
+            displayText.textContent = displayValue;
+        }else{
+            currentValue = operate(+currentValue, +input, currentOperator);
+            displayTextTop.textContent = currentValue + " " + OPERATORS[button.id];
+            displayValue = "";
+            displayText.textContent = displayValue;
+        }
+        currentOperator = button.id
+    
     }
 }))
